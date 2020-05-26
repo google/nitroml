8be6b5abcdbed97b40ff1f55d3dd5fbf7d31084e
Charles Weill <weill@google.com>
2020-05-26 23:49:46 +0000

    Add Apache 2 header to Python files.
    Add README.
    
    PiperOrigin-RevId: 313292960

---
+++ README.md
@@ -0,0 +1,14 @@
+# NitroML
+
+**NitroML** is a framework for benchmarking machine learning
+model-quality.
+
+Our mission is to provide _model-quality benchmarking tools to accelerate
+machine learning research and development_.
+
+More generally, NitroML enables machine learning research teams to iterate more
+quickly on their highly-customized, heterogeneous, and multi-stage pipelines. It
+offers machine learning model-quality benchmarking best practices
+out-of-the-box, curates raw datasets, and leverages [TFX:OSS] to
+scale with Cloud-resources. Its benchmark database and analysis tools ensure
+that AutoML teams can be data-driven as they modify their systems.
--- nitroml.py
+++ nitroml.py
@@ -1,7 +1,20 @@
+# Copyright 2020 Google LLC
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     https://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 # Lint as: python3
 """NitroML: Accelerate AutoML development.
 
-go/nitroml-docs
+go/nitroml
 
 NitroML is a framework for benchmarking AutoML workflows composed of
 TFX OSS and MyOrchestrator components.
@@ -15,8 +28,6 @@ data-driven as they modify their systems.
 NitroML's API is inspired by the Python stdlib's `unittest` package. It
 is intended to encourage writing benchmarks in a similar manner to writing
 test cases.
-
-**HIGHLY EXPERIMENTAL AND API SUBJECT TO CHANGE**
 """
 
 import abc
