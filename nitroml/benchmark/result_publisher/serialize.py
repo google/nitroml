bb1b64e9526f11d5da97d090ddcaea47cc8fe95d
Jose Figueroa <serniebanders@google.com>
2020-12-10 23:06:43 +0000

    Add serializing methods for context property dictionaries.
    * Utilizes json serialization with specific type checking for primitive data types.
    
    PiperOrigin-RevId: 346873747

---
+++ nitroml/benchmark/result_publisher/serialize.py
@@ -0,0 +1,43 @@
+"""Serialization methods for context properties."""
+
+import json
+from typing import Dict, Union
+
+
+def _type_check(properties):
+  for key, value in properties.items():
+    if not isinstance(key, str):
+      raise TypeError('Key (%s}) is of type (%s), str required' %
+                      (str(key), type(key)))
+    if not isinstance(value, (int, float, str)):
+      raise TypeError(
+          'Value (%s}) is of type (%s), int, float, or str required' %
+          (str(value), type(value)))
+
+
+def decode(properties: str) -> Dict[str, Union[int, float, str]]:
+  """Converts a serialized property dict into a python dict.
+
+  Args:
+    properties: A string rep of a properties dictionary encoded by encode().
+  Returns:
+    Dict representation of properties.
+  """
+  return json.loads(properties)
+
+
+def encode(properties: Dict[str, Union[int, float, str]]) -> str:
+  """Convert a property dict to a serializeabled string.
+
+  Args:
+    properties: A dictionary of string keys and primitive datatype values.
+  Returns:
+    A serialized json string rep of properties.
+
+  Raises:
+    TypeError: If any of the values in the dictionary are not serializeable.
+    Current support for int, float, and string datatypes
+  """
+  _type_check(properties)
+  return json.dumps(properties, sort_keys=True)
+
---
+++ nitroml/benchmark/result_publisher/serialize_test.py
@@ -0,0 +1,31 @@
+"""Tests for google3.third_party.py.nitroml.benchmark.result_publisher.serialize."""
+
+from absl.testing import absltest
+from nitroml.benchmark.result_publisher import serialize
+
+
+class SerializeTest(absltest.TestCase):
+
+  def testEncodeFunctionTypeCheck(self):
+    properties1 = {'list': [], 'int': 1, 'str': 'string'}
+    properties2 = {'int': 1, 'dict': dict(), 'float': 0.1}
+    properties3 = {'int': 1, 'float': 0.1, 'str': 'string'}
+    with self.assertRaises(TypeError):
+      serialize.encode(properties1)
+
+    with self.assertRaises(TypeError):
+      serialize.encode(properties2)
+
+    want = '{"float": 0.1, "int": 1, "str": "string"}'
+    got = serialize.encode(properties3)
+    self.assertEqual(want, got)
+
+  def testDecode(self):
+    want = {'int': 1, 'float': 0.1, 'str': 'string'}
+    encoded_properties = serialize.encode(want)
+    got = serialize.decode(encoded_properties)
+    self.assertCountEqual(want, got)
+
+
+if __name__ == '__main__':
+  absltest.main()
